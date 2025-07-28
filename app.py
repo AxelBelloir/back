6e53194d463ce2231e3a1ffc74bc6650ec24e3c1
AxelBelloir <axelbelloir1@gmail.com>
2025-07-28 20:01:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+def acces_compte(id,mp):
+    import sqlite3
+    conn = sqlite3.connect("COMPTE")
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS compte (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    id TEXT,
+    mp TEXT,
+    data TEXT,
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -7,8 +16,10 @@ CORS(app)  # Important pour autoriser ton frontend à appeler l'API
 @app.route('/api/greet', methods=['POST'])
 def greet():
     data = request.get_json()
-    name = data.get('name', 'inconnu')
-    return jsonify({'message': f'Bonjour, {name} !'})
+    id = data.get('name', 'inconnu')
+    mp = data.get('mp', 'inconnu')
+    message = acces_compte(name,mp)
+    return jsonify({'message': f'Bonjour, {return_message}'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
