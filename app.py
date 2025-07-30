81b2221c8a6cd9d28ce960c2f458b15535f02df8
AxelBelloir <axelbelloir1@gmail.com>
2025-07-30 18:17:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,14 @@ def acces_compte(demande):
             if compte[index][1] == demande[1]:
                 if compte[index][2] == demande[2]:
                     message = "Connexion reussi."
+                    conn.commit
+                    conn.close
                     return message
             index += 1
         message = "Identifiant ou mot-de-passe incorecte."
         return message
+        conn.commit
+        conn.close
     elif demande[0] == 1:
         cursor.execute("SELECT * FROM compte")
         compte = cursor.fetchall()
@@ -32,8 +36,12 @@ def acces_compte(demande):
         if out == False:
             cursor.execute("INSERT INTO compte (id,mp) VALUES (?,?)",(demande[1],demande[2]))
             return "Compte creer"
+            conn.commit
+            conn.close
         else:
             return "Cette identifiant est deja utilisée."
+            conn.commit
+            conn.close
             
 from flask import Flask, request, jsonify
 from flask_cors import CORS
